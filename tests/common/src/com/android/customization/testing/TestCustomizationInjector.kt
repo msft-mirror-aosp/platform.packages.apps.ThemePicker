45308198182b9702066df5c9baf5c2d36e4d0c15
Dave Mankoff <mankoff@google.com>
2024-11-11 18:12:03 +0000

    Replace field injection with constructor  injection.
    Change-Id: I86ef6b5a23777bd9531d13e2d6b86b3c0f981e80
    
    ---
    THIS CL MAY BRIEFLY, INTENTIONALLY, BREAK THE BUILD.
    
    This CL corresponds with two CLs in aosp:
    https://r.android.com/q/topic:%22k2-upgrade-wpp%22
    
    Once all three CLs are in, the build will straighten itself out.
    It is not currenlty possible to submit all three atomically, thus
    the possibility of a broken build.
    
    PLEASE BE PATIENT BEFORE REVERTING. If all three CLs are merged,
    please wait an hour to see if the build goes green before reverting.
    ---
    
    This fixes an issue we are having with an upgrade to Kotlin 2.0.21.
    
    See the presubmits on https://r.android.com/3290825.
    
    Generally, field injection should be avoided in favor of constructor
    injection, so hopefully this is a change for the better.
    
    Bug: 369137871
    Flag: EXEMPT Minor refactor
    Test: m ThemePickerApplicationLib
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:65d5aa7e1169340dd1be22d892ba26a34050eb52)
    Merged-In: I718113e103da303ddfb102b73c95f9a9b25da132
    Change-Id: I718113e103da303ddfb102b73c95f9a9b25da132
    
    Change-Id: I458c01e4b1a22d6732d1e16321903d127150cba6

--- tests/common/src/com/android/customization/testing/TestCustomizationInjector.kt
+++ tests/common/src/com/android/customization/testing/TestCustomizationInjector.kt
@@ -16,9 +16,16 @@ import com.android.customization.picker.color.domain.interactor.ColorPickerInter
 import com.android.customization.picker.color.ui.viewmodel.ColorPickerViewModel
 import com.android.customization.picker.quickaffordance.domain.interactor.KeyguardQuickAffordancePickerInteractor
 import com.android.systemui.shared.clocks.ClockRegistry
+import com.android.wallpaper.module.NetworkStatusNotifier
+import com.android.wallpaper.module.PartnerProvider
+import com.android.wallpaper.module.WallpaperPreferences
 import com.android.wallpaper.module.logging.UserEventLogger
+import com.android.wallpaper.network.Requester
 import com.android.wallpaper.picker.customization.data.repository.WallpaperColorsRepository
+import com.android.wallpaper.picker.customization.domain.interactor.WallpaperInteractor
+import com.android.wallpaper.testing.FakeWallpaperClient
 import com.android.wallpaper.testing.TestInjector
+import com.android.wallpaper.util.DisplayUtils
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -27,8 +34,26 @@ open class TestCustomizationInjector
 @Inject
 constructor(
     private val customPrefs: TestDefaultCustomizationPreferences,
-    private val themesUserEventLogger: ThemesUserEventLogger
-) : TestInjector(themesUserEventLogger), CustomizationInjector {
+    private val themesUserEventLogger: ThemesUserEventLogger,
+    displayUtils: DisplayUtils,
+    requester: Requester,
+    networkStatusNotifier: NetworkStatusNotifier,
+    partnerProvider: PartnerProvider,
+    wallpaperClient: FakeWallpaperClient,
+    injectedWallpaperInteractor: WallpaperInteractor,
+    prefs: WallpaperPreferences,
+) :
+    TestInjector(
+        themesUserEventLogger,
+        displayUtils,
+        requester,
+        networkStatusNotifier,
+        partnerProvider,
+        wallpaperClient,
+        injectedWallpaperInteractor,
+        prefs,
+    ),
+    CustomizationInjector {
     /////////////////
     // CustomizationInjector implementations
     /////////////////
@@ -53,7 +78,7 @@ constructor(
 
     override fun getWallpaperColorResources(
         wallpaperColors: WallpaperColors,
-        context: Context
+        context: Context,
     ): WallpaperColorResources {
         throw UnsupportedOperationException("not implemented")
     }
