7f44ad15cb10c1120493b9a8607606a7b4980590
Hawkwood Glazier <jglazier@google.com>
2024-11-21 22:38:23 +0000

    Make resource functions somewhat more resuable
    Bug: 364680879
    Test: Manual + Presubmits
    Flag: NONE Interface refactoring
    Change-Id: I6ce377475662d0802f8e2838e2b5ce800345fb23

--- src/com/android/wallpaper/customization/ui/binder/ThemePickerCustomizationOptionBinder.kt
+++ src/com/android/wallpaper/customization/ui/binder/ThemePickerCustomizationOptionBinder.kt
@@ -325,7 +325,7 @@ constructor(private val defaultCustomizationOptionsBinder: DefaultCustomizationO
                                     // TODO(b/379348167): get correct isShadeLayoutWide from picker
                                     clockController.largeClock.layout.applyPreviewConstraints(
                                         ClockPreviewConfig(
-                                            previewContext = context,
+                                            context = context,
                                             isShadeLayoutWide = false,
                                             isSceneContainerFlagEnabled = false,
                                         ),
@@ -333,7 +333,7 @@ constructor(private val defaultCustomizationOptionsBinder: DefaultCustomizationO
                                     )
                                     clockController.smallClock.layout.applyPreviewConstraints(
                                         ClockPreviewConfig(
-                                            previewContext = context,
+                                            context = context,
                                             isShadeLayoutWide = false,
                                             isSceneContainerFlagEnabled = false,
                                         ),
