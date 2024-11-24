c7865d33645c4307ad7cd2536501c7f610825d39
Bhavuk Jain <bhavukj@google.com>
2024-11-24 00:27:13 +0000

    Add dependencies for curated photos in themepicker
    This CL aims at adding dependencies for curated photos in themepicker.
    
    Bug: 370078035
    Flag: com.android.systemui.shared.new_customization_picker_ui
    Test: Tested by running on local, and checking if curated photos are
    available and can be set
    
    Change-Id: Ic4de4f5c051c43e00f8a08f27b76d0d4e423784d

--- tests/module/src/com/android/wallpaper/ThemePickerTestModule.kt
+++ tests/module/src/com/android/wallpaper/ThemePickerTestModule.kt
@@ -48,6 +48,7 @@ import com.android.wallpaper.module.logging.UserEventLogger
 import com.android.wallpaper.modules.ThemePickerAppModule
 import com.android.wallpaper.network.Requester
 import com.android.wallpaper.picker.category.domain.interactor.CategoryInteractor
+import com.android.wallpaper.picker.category.domain.interactor.CuratedPhotosInteractor
 import com.android.wallpaper.picker.category.domain.interactor.ThirdPartyCategoryInteractor
 import com.android.wallpaper.picker.category.ui.view.providers.IndividualPickerFactory
 import com.android.wallpaper.picker.category.ui.view.providers.implementation.DefaultIndividualPickerFactory
@@ -62,6 +63,7 @@ import com.android.wallpaper.picker.di.modules.MainDispatcher
 import com.android.wallpaper.picker.preview.ui.util.DefaultImageEffectDialogUtil
 import com.android.wallpaper.picker.preview.ui.util.ImageEffectDialogUtil
 import com.android.wallpaper.testing.FakeCategoryInteractor
+import com.android.wallpaper.testing.FakeCuratedPhotosInteractorImpl
 import com.android.wallpaper.testing.FakeDefaultRequester
 import com.android.wallpaper.testing.FakeThirdPartyCategoryInteractor
 import com.android.wallpaper.testing.FakeWallpaperCategoryWrapper
@@ -137,6 +139,12 @@ abstract class ThemePickerTestModule {
     @Binds
     @Singleton
     abstract fun bindCategoryInteractor(impl: FakeCategoryInteractor): CategoryInteractor
+
+    @Binds
+    @Singleton
+    abstract fun bindCuratedPhotosInteractor(
+        impl: FakeCuratedPhotosInteractorImpl
+    ): CuratedPhotosInteractor
 
     @Binds @Singleton abstract fun bindInjector(impl: TestCustomizationInjector): Injector
 
