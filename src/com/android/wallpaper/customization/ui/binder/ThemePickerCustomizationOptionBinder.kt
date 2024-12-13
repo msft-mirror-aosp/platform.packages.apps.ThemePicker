99cf24154ac9c2a2033c58b8f38c0821b1d7c3b0
George Lin <giolin@google.com>
2024-12-13 17:41:30 +0000

    Navigate to more lock screen settings (2/2)
    Enable the entry to lead to the more lock screen settings screen
    
    Test: Manuallt tested
    Fixes: 376090582
    Flag: com.android.systemui.shared.new_customization_picker_ui
    Change-Id: I5346791374202ad1ac1af8cad82b4159e2599004

--- src/com/android/wallpaper/customization/ui/binder/ThemePickerCustomizationOptionBinder.kt
+++ src/com/android/wallpaper/customization/ui/binder/ThemePickerCustomizationOptionBinder.kt
@@ -69,6 +69,7 @@ constructor(private val defaultCustomizationOptionsBinder: DefaultCustomizationO
         colorUpdateViewModel: ColorUpdateViewModel,
         lifecycleOwner: LifecycleOwner,
         navigateToWallpaperCategoriesScreen: (screen: Screen) -> Unit,
+        navigateToMoreLockScreenSettingsActivity: () -> Unit,
     ) {
         defaultCustomizationOptionsBinder.bind(
             view,
@@ -79,6 +80,7 @@ constructor(private val defaultCustomizationOptionsBinder: DefaultCustomizationO
             colorUpdateViewModel,
             lifecycleOwner,
             navigateToWallpaperCategoriesScreen,
+            navigateToMoreLockScreenSettingsActivity,
         )
 
         val optionClock =
@@ -103,6 +105,14 @@ constructor(private val defaultCustomizationOptionsBinder: DefaultCustomizationO
             optionShortcut?.findViewById<ImageView>(
                 R.id.option_entry_keyguard_quick_affordance_icon_2
             )
+
+        val optionMoreLockScreenSettings =
+            lockScreenCustomizationOptionEntries
+                .find { it.first == ThemePickerLockCustomizationOption.MORE_LOCK_SCREEN_SETTINGS }
+                ?.second
+        optionMoreLockScreenSettings?.setOnClickListener {
+            navigateToMoreLockScreenSettingsActivity.invoke()
+        }
 
         val optionColors =
             homeScreenCustomizationOptionEntries
